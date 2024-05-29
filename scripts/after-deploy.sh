96533f0bf9545b49cecf11219021c8a126b31c6a
Solmi <7548ck@naver.com>
2024-05-30 02:00:49 +0900

    chore: after-deploy.sh 에러 해결

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -6,9 +6,5 @@ cd $REPOSITORY
 # npm을 사용하여 패키지 설치
 sudo /usr/bin/npm install
 
-# Prisma 사용 시 추가 명령어 실행
-sudo /usr/bin/npm run db:pull # 해당 명령어는 Prisma를 사용하는 경우에만 실행
-sudo /usr/bin/npm run generate # 해당 명령어는 Prisma를 사용하는 경우에만 실행
-
 # pm2를 사용하여 애플리케이션 시작
 sudo /usr/bin/pm2 start dist
