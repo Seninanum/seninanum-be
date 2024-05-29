fc2c571ec23575123d582cd6ab26a55de15c0fa9
Solmi <7548ck@naver.com>
2024-05-30 03:22:12 +0900

    feat: 새로운 테스트 api 코드

--- routes/test.js
+++ routes/test.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("Hello from test router!");
+  res.send("Hello from new test router!");
 });
 
 module.exports = router;
--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -3,8 +3,10 @@ REPOSITORY=/home/ubuntu/build
 
 cd $REPOSITORY
 
+git pull origin main
+
 # npm을 사용하여 패키지 설치
 sudo /usr/bin/npm install
 
-# pm2를 사용하여 애플리케이션 시작
+# pm2를 사용하여 애플리케이션 재시작
 sudo pm2 restart all
