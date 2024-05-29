600e4d45b3d1a8c4c3d3d6bf1e56f3d78298f669
Solmi <7548ck@naver.com>
2024-05-30 01:55:02 +0900

    chore: after-deploy.sh 파일 수정

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -3,7 +3,12 @@ REPOSITORY=/home/ubuntu/build
 
 cd $REPOSITORY
 
-sudo /usr/bin/yarn
-sudo /usr/bin/yarn db:pull //prisma 사용했을 경우만
-sudo /usr/bin/yarn generate //prisma 사용했을 경우만 - 해당명령어는 위의 scripts의 명령어임
+# npm을 사용하여 패키지 설치
+sudo /usr/bin/npm install
+
+# Prisma 사용 시 추가 명령어 실행
+sudo /usr/bin/npm run db:pull # 해당 명령어는 Prisma를 사용하는 경우에만 실행
+sudo /usr/bin/npm run generate # 해당 명령어는 Prisma를 사용하는 경우에만 실행
+
+# pm2를 사용하여 애플리케이션 시작
 sudo /usr/bin/pm2 start dist
