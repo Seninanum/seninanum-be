dab9e0186dec0fa0f79f74d16dc883e838de7418
Solmi <7548ck@naver.com>
2024-06-05 03:18:40 +0900

    docs: 디렉토리 구조 변경

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-var testRouter = require("./routes/test");
+var indexRouter = require("./src/routes/index");
+var usersRouter = require("./src/routes/users");
+var testRouter = require("./src/routes/test");
 
 var app = express();
 
