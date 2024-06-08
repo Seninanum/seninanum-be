dbffe3f059b40d88d8c5636d09451dee20d2d0c3
Solmi <7548ck@naver.com>
2024-06-08 21:24:40 +0900

    feat: 데이터 베이스 지움

--- src/app.js
+++ src/app.js
@@ -1,12 +1,8 @@
 // import
 const express = require("express");
 const morgan = require("morgan");
-const { sequelize } = require("./models");
-// const kakao = require("../src/passport/kakaoStrategy");
-// const passport = require("passport");
 const path = require("path");
 const cookieParser = require("cookie-parser");
-// const session = require("express-session");
 const cors = require("cors");
 
 // router
@@ -16,15 +12,6 @@ const testRouter = require("./routes/test");
 
 const app = express();
 app.set("port", process.env.PORT || 3001);
-
-sequelize
-  .sync({ force: true })
-  .then(() => {
-    console.log("데이터베이스 연결 성공");
-  })
-  .catch((err) => {
-    console.error(err);
-  });
 
 app.use(
   cors({
@@ -42,41 +29,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(express.static(path.join(__dirname, "public")));
-
-// app.use(
-//   session({
-//     secret: process.env.SESSION_SECRET,
-//     resave: false,
-//     saveUninitialized: false,
-//     cookie: {
-//       domain: " ",
-//       path: "/",
-//       secure: false,
-//       httpOnly: false,
-//     },
-//   })
-// );
-
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// passport.serializeUser((token, done) => {
-//   done(null, token);
-// });
-
-// passport.deserializeUser((token, done) => {
-//   const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//   const userId = decoded.userId;
-
-//   Users.findByPk(userId)
-//     .then((user) => {
-//       done(null, user);
-//     })
-//     .catch((err) => {
-//       done(err);
-//     });
-// });
-// kakao();
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
