b53e7b3f58a076916eb2a13fb74bdb90d3e7b492
Solmi <7548ck@naver.com>
2024-06-08 15:53:28 +0900

    fix: ecosystem에 cwd 추가

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,6 +7,7 @@ module.exports = {
       autorestart: true,
       watch: false,
       max_memory_restart: "1G",
+      cwd: "/home/ubuntu/build",
       env: {
         NODE_ENV: "development",
         PORT: 3001,
