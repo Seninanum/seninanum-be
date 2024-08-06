95167ab26b531af43ec5c6c69447f871cde429c8
Solmi <7548ck@naver.com>
2024-08-06 21:54:11 +0900

    chore: build error 해결

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -7,7 +7,7 @@ cd $REPOSITORY
 npm install
 
 # Start or restart the application with PM2, updating environment variables
-pm2 start build/src/app.js --name 'seninanum-be'
+pm2 start src/app.js --name "" --update-env
 
 # Save the PM2 process list and corresponding environments
 pm2 save
