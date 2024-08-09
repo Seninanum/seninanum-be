69be2c3dbb3bb9af38a59e5c9ab280b5b349c40b
Solmi <7548ck@naver.com>
2024-08-10 02:56:44 +0900

    chore: build error 해결

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 REPOSITORY=/home/ubuntu/build
 
-echo "Changing directory to $REPOSITORY"
-cd $REPOSITORY || exit
+cd $REPOSITORY
 
-echo "Installing dependencies"
+# Install dependencies
 npm install
 
-echo "Starting the application with PM2"
-pm2 start src/app.js --name "seninanum" --update-env
+# Start or restart the application with PM2, updating environment variables
+pm2 start src/app.js --name "" --update-env
 
-echo "Saving PM2 process list"
+# Save the PM2 process list and corresponding environments
 pm2 save
 
-echo "After deploy script executed successfully"
