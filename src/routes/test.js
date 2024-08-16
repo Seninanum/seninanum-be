51284037922a636f7d92bc0bfce7bfbec2aae6ad
Solmi <7548ck@naver.com>
2024-08-17 00:14:40 +0900

    fix: 회원가입 CORS 에러 해결

--- src/routes/test.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (req, res) => {
-  res.send("Hello from 되라되라 test router!");
-});
-
-module.exports = router;
