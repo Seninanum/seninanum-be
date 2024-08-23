02695ac48376bb5c6006a6d5863da753a3eb7708
Solmi <7548ck@naver.com>
2024-08-23 23:51:32 +0900

    test: after_deploy test

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 REPOSITORY=/home/ubuntu/build
-
 cd $REPOSITORY
 
-# Remove node_modules and package-lock.json
-rm -rf node_modules
-rm -f package-lock.json
-
-# Install dependencies
 npm install
+npm start
 
-# Restart the application (if needed)
-pm2 restart ecosystem.config.js --env production
