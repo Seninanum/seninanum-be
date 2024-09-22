09d0ab469a16f6f041816a325e1e8001bd15832d
Solmi <7548ck@naver.com>
2024-09-22 16:57:47 +0900

    feat: 경력 프로필 생성 구현

--- src/routes/register/career.js
+++ src/routes/register/career.js
@@ -1,3 +1,4 @@
+const e = require("express");
 const express = require("express");
 const router = express.Router();
 const pool = require("../../database/db");
@@ -5,28 +6,35 @@ const pool = require("../../database/db");
 router.post("/", async (req, res) => {
   /**
    * #swagger.tags = ['Career']
-   * #swagger.summary = '경력 프로필 등록'
-   * #swagger.description = '경력 프로필 등록하기'
+   * #swagger.summary = '경력 프로필 생성'
+   * #swagger.description = '경력 프로필 생성하기'
    */
-  const { introduce, age, field, service, method, region, priceType, price } =
-    req.body;
 
+  //생성되어있는 프로필이 있는지 확인
   const userId = req.user.userId;
-
   try {
-    const [user] = await pool.query("SELECT * FROM user WHERE userId = ?", [
-      userId,
-    ]);
-    if (user.length === 0) {
-      return res.status(400).json({ error: "유효하지 않은 userId입니다." });
-    }
-
-    const [result] = await pool.query(
-      "INSERT INTO careerProfile (userId, introduce, age, field, service, method, region, priceType, price) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
-      [userId, introduce, age, field, service, method, region, priceType, price]
+    const [existingProfile] = await pool.query(
+      "SELECT * FROM careerProfile WHERE userId = ?",
+      [userId]
     );
-
-    res.status(201).json({ message: "경력프로필이 등록되었습니다." });
+    if (existingProfile.length === 0) {
+      // 프로필 새로 생성
+      const [result] = await pool.query(
+        "INSERT INTO careerProfile (userId, introduce, age, field, service, method, region, priceType, price) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+        [userId, null, null, null, null, null, null, null, null]
+      );
+      // 생성된 프로필의 ID 반환
+      const profileId = result.insertId;
+      return res
+        .status(201)
+        .json({ profileId, message: "새 프로필이 생성되었습니다." });
+    } else {
+      // 기존 프로필 ID 반환
+      const profileId = existingProfile[0].profileId;
+      return res
+        .status(200)
+        .json({ profileId, message: "기존 프로필이 반환되었습니다." });
+    }
   } catch (error) {
     console.log(error);
     res
@@ -35,47 +43,6 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.get("/list", async (req, res) => {
-  /**
-   * #swagger.tags = ['Career']
-   * #swagger.summary = '경력 프로필 조회'
-   * #swagger.description = '경력 프로필 조회하기'
-   */
-  try {
-    //경력프로필 정보
-    const [careers] = await pool.query(
-      "SELECT profileId, userId, introduce, age, field FROM careerProfile"
-    );
-
-    const careerWithUserInfo = await Promise.all(
-      careers.map(async (career) => {
-        const [user] = await pool.query(
-          "SELECT nickname, gender, birthyear FROM user WHERE userId = ?",
-          [career.userId]
-        );
-        const { nickname, gender, birthyear } = user[0];
-        return {
-          profileId: career.profileId,
-          introduce: career.introduce,
-          age: career.age,
-          field: career.field,
-          nickname,
-          gender,
-          birthyear,
-        };
-      })
-    );
-
-    res.status(200).json(careerWithUserInfo);
-  } catch (error) {
-    console.log(error);
-    res
-      .status(500)
-      .json({ error: "An error occurred while fetching career list" });
-  }
-});
-
-// 경력프로필 상세정보 불러오기
 router.get("/:profileId", async (req, res) => {
   /**
    * #swagger.tags = ['Career']
@@ -83,6 +50,12 @@ router.get("/:profileId", async (req, res) => {
    * #swagger.description = '경력 프로필 상세항목 조회하기'
    */
   const profileId = req.params.profileId;
+
+  const userId = req.user.userId;
+
+  // userId가 careerProfile에 있으면 해당 행을 반환
+
+  // userId가 careerProfile에 없으면 새로 생성
 
   try {
     const [career] = await pool.query(
@@ -134,4 +107,91 @@ router.get("/:profileId", async (req, res) => {
       .json({ error: "An error occurred while fetching recruit detail" });
   }
 });
+
+//patch으로 변경
+router.patch("/", async (req, res) => {
+  /**
+   * #swagger.tags = ['Career']
+   * #swagger.summary = '경력 프로필 등록'
+   * #swagger.description = '경력 프로필 등록하기'
+   */
+
+  const {
+    profileId,
+    introduce,
+    age,
+    field,
+    service,
+    method,
+    region,
+    priceType,
+    price,
+  } = req.body;
+
+  try {
+    // profileId인 곳에 프로필 Update
+    const [result] = await pool.query(
+      "UPDATE careerProfile SET introduce = ?, age = ?, field = ?, service = ?, method = ?, region = ?, priceType = ?, price = ? WHERE profileId = ?",
+      [
+        introduce,
+        age,
+        field,
+        service,
+        method,
+        region,
+        priceType,
+        price,
+        profileId,
+      ]
+    );
+
+    res.status(201).json({ message: "경력프로필이 업데이트 되었습니다." });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while creating the careerProfile" });
+  }
+});
+
+// router.get("/list", async (req, res) => {
+//   /**
+//    * #swagger.tags = ['Career']
+//    * #swagger.summary = '경력 프로필 조회'
+//    * #swagger.description = '경력 프로필 조회하기'
+//    */
+//   try {
+//     //경력프로필 정보
+//     const [careers] = await pool.query(
+//       "SELECT profileId, userId, introduce, age, field FROM careerProfile"
+//     );
+
+//     const careerWithUserInfo = await Promise.all(
+//       careers.map(async (career) => {
+//         const [user] = await pool.query(
+//           "SELECT nickname, gender, birthyear FROM user WHERE userId = ?",
+//           [career.userId]
+//         );
+//         const { nickname, gender, birthyear } = user[0];
+//         return {
+//           profileId: career.profileId,
+//           introduce: career.introduce,
+//           age: career.age,
+//           field: career.field,
+//           nickname,
+//           gender,
+//           birthyear,
+//         };
+//       })
+//     );
+
+//     res.status(200).json(careerWithUserInfo);
+//   } catch (error) {
+//     console.log(error);
+//     res
+//       .status(500)
+//       .json({ error: "An error occurred while fetching career list" });
+//   }
+// });
+
 module.exports = router;
