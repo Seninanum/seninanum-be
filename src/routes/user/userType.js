94ddff5b9f4b4b1c1e6728525a3de65c1b944573
Solmi <7548ck@naver.com>
2024-09-25 04:08:36 +0900

    feat: 경력 프로필 작성 단계 api

--- src/routes/user/userType.js
+++ src/routes/user/userType.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { pool } = require("../../database/db");
 const router = express.Router();
 
 router.get("/userType", async (req, res) => {
@@ -7,7 +8,27 @@ router.get("/userType", async (req, res) => {
     #swagger.summary = '유저 타입 불러오기'
    */
   const user = req.user;
-  res.json(user.userType);
+
+  try {
+    const [career] = await pool.query(
+      "SELECT progressStep FROM careerProfile WHERE userid = ?",
+      [user.userId]
+    );
+
+    // career가 빈 배열일 경우 처리
+    const result = career[0];
+    if (career.length > 0) {
+      result = -1;
+    }
+
+    // 응답 형식화
+    res.json({ userType: user.userType, career: result });
+  } catch (error) {
+    console.error("Error fetching user type:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while fetching user type." });
+  }
 });
 
 module.exports = router;
