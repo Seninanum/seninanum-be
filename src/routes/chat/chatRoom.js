1373ec9c9b3f4cc958dcb194ff667f923d2a4c41
Solmi <7548ck@naver.com>
2024-10-09 19:40:28 +0900

    feat: 채팅방 목록 조회 api 구현

--- src/routes/chat/chatRoom.js
+++ src/routes/chat/chatRoom.js
@@ -18,13 +18,6 @@ router.post("/create", async (req, res) => {
     });
   }
 
-  // 방 이름
-  const [roomNames] = await pool.query(
-    "SELECT * FROM profile WHERE profileId = ?",
-    [myProfileId]
-  );
-  const roomName = roomNames[0].nickname;
-
   // 생성되어있는 방이 있는지 확인
   try {
     const [existingChatroom] = await pool.query(
@@ -35,8 +28,8 @@ router.post("/create", async (req, res) => {
     if (existingChatroom.length === 0) {
       // 새로 프로필 생성
       const [result] = await pool.query(
-        "INSERT INTO chatRoom (roomName, roomStatus, memberId, opponentId) VALUES (?, ?, ?, ?)",
-        [roomName, "ACTIVE", myProfileId, oppProfileId]
+        "INSERT INTO chatRoom (roomStatus, memberId, opponentId) VALUES (?, ?, ?, ?)",
+        ["ACTIVE", myProfileId, oppProfileId]
       );
 
       // 생성된 채팅방의 ID 반환
@@ -62,6 +55,57 @@ router.post("/create", async (req, res) => {
   }
 });
 
-router.get("/memberIds/");
+router.get("/list", async (req, res) => {
+  /**
+    #swagger.tags = ['Chatroom']
+    #swagger.summary = '채팅 방 목록 조회'
+  */
+
+  const myProfileId = req.user.profileId;
+
+  try {
+    // chatRoom에 해당하는 방 모두 가져오기
+    const [existingChatroom] = await pool.query(
+      "SELECT * FROM chatRoom WHERE memberId = ? OR opponentId = ?",
+      [myProfileId, myProfileId]
+    );
+
+    // 채팅 목록 없음
+    if (existingChatroom.length === 0) {
+      return res.status(404).json({ message: "생성된 채팅방이 없습니다." });
+    }
+
+    // 사용자 기준으로 응답값 수정
+    const modifiedChatrooms = await Promise.all(
+      existingChatroom.map(async (room) => {
+        // myProfileId가 memberId와 같은지 확인
+        const opponentId =
+          room.memberId === myProfileId ? room.opponentId : room.memberId;
+
+        // 방 이름 (상대방 닉네임)
+        const [roomNames] = await pool.query(
+          "SELECT nickname FROM profile WHERE profileId = ?",
+          [opponentId]
+        );
+        const roomName = roomNames[0]?.nickname || "Unknown";
+
+        return {
+          chatRoomId: room.chatRoomId,
+          roomName: roomName,
+          roomStatus: room.roomStatus,
+          opponentId: opponentId,
+          createdAt: room.createdAt,
+        };
+      })
+    );
+
+    return res.status(200).json(modifiedChatrooms);
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while creating the chatRoom" });
+  }
+});
 
 module.exports = router;
