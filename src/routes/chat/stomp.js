3b36dab34aa541aba47abe20b873fd18cb3b2e45
Solmi <7548ck@naver.com>
2024-10-15 05:58:48 +0900

    feat: 메세지 전송 구현

--- src/routes/chat/stomp.js
+++ src/routes/chat/stomp.js
@@ -1,4 +1,7 @@
 const StompServer = require("stomp-broker-js");
+const express = require("express");
+const router = express.Router();
+const pool = require("../../database/db");
 
 module.exports = function (server) {
   // STOMP 서버 설정
@@ -22,25 +25,25 @@ module.exports = function (server) {
 
   // 클라이언트가 메시지를 보낼 때
   stompServer.on("send", (dest, frame) => {
-    console.log(`Destination: ${JSON.stringify(dest)}`);
-    // Destination: {"frame":{"headers":{"destination":"/app/chat/11","content-length":68,"bytes_message":true},"body":"{\"chatMessage\":\"d\",\"senderId\":8,\"receiverId\":7,\"publishType\":\"USER\"}"},"dest":"/app/chat/11"}
-
     const destination = dest.frame.headers.destination; // 메시지가 보내진 경로
     const messageBody = JSON.parse(dest.frame.body); // 메시지 본문
+    const roomId = destination.split('/').pop();
 
-    console.log("확인>>>>>>>>>", destination, messageBody);
+    // DB에 저장
+    await pool.query(
+      "INSERT INTO chatMessage (chatRoomId, senderId, chatMessage, senderType, unreadCount) VALUES (?, ?, ?, ?, ?)",
+      [roomId, messageBody.senderId, messageBody.chatMessage, messageBody.publishType, 1]
+    );
 
-    // if (destination.startsWith("/app/chat/")) {
-    //   // 경로가 "/app/chat/{roomId}"로 시작하는 경우
-    //   const roomId = destination.split("/")[3]; // roomId 추출
-
-    //   // 해당 roomId에 있는 모든 클라이언트에게 메시지 브로드캐스트
-    //   stompServer.send(
-    //     `/topic/chat/${roomId}`,
-    //     headers,
-    //     JSON.stringify(messageBody)
-    //   );
-    // }
+    // 메세지 전달
+    if (destination.startsWith("/app/chat/")) {
+      // 해당 roomId에 있는 클라이언트에게 메시지 브로드캐스트
+      stompServer.send(
+        `/topic/chat/${roomId}`,
+        {},
+        JSON.stringify(messageBody)
+      );
+    }
   });
 
   // 클라이언트가 연결을 끊을 때
