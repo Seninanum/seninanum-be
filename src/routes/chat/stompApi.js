5ad5dbc15e4dc56ed7b297adf7d0b99d129cfe11
Solmi <7548ck@naver.com>
2024-10-17 00:22:11 +0900

    fix: 테스트 오류 해결

--- src/routes/chat/stompApi.js
+++ src/routes/chat/stompApi.js
@@ -6,9 +6,13 @@ router.post("/disconnect", async (req, res) => {
   const { roomId, memberId, lastReadMessageId } = req.body;
 
   try {
-    console.log(req.body);
+    console.log(roomId, memberId, lastReadMessageId);
+    return res.status(200).json({ message: "수신성공" });
   } catch (error) {
     console.log(error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while creating the chatRoom" });
   }
 });
 
