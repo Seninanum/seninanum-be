c12f434195651f0510f951543e61535c7694b1db
Solmi <7548ck@naver.com>
2024-11-07 03:00:53 +0900

    feat: s3 링크 반환 api 구현

--- src/app.js
+++ src/app.js
@@ -25,6 +25,7 @@ const applicationRouter = require("./routes/application/application");
 const matchRouter = require("./routes/match/match");
 const freeBoardRouter = require("./routes/board/freeBoard");
 const adviceBoardRouter = require("./routes/board/adviceBoard");
+const imageRouter = require("./routes/aws/s3Image");
 
 // swagger
 const swaggerUi = require("swagger-ui-express");
@@ -71,6 +72,7 @@ app.use("/application", verifyToken, applicationRouter);
 app.use("/match", verifyToken, matchRouter);
 app.use("/board/advice", verifyToken, adviceBoardRouter);
 app.use("/board/free", verifyToken, freeBoardRouter);
+app.use("/", verifyToken, imageRouter);
 
 app.use(
   "/api-docs",
